2a7dd078d84102f38e63433555f853bd07882817
Christian Paul <christiandecembrana1@gmail.com>
2022-05-01 21:35:06 +0800

    rename and added to collection

--- src/app/components/collection/collection_list.ts
+++ src/app/components/collection/collection_list.ts
@@ -2,42 +2,37 @@ export const COLLECTION_LIST = {
   collections: [
     {
       src: "assets/img/collections/agostea.jpg",
-      title: "Disco #1",
-      hovered: false,
-    },
-    {
-      src: "assets/img/collections/ceej.jpg",
-      title: "CEEJ Portfolio #1",
+      title: "Disco - Agostea",
       hovered: false,
     },
     {
       src: "assets/img/collections/commerce.jpg",
-      title: "CEEJ Portfolio #2",
+      title: "CEEJ Website",
       hovered: false,
     },
     {
       src: "assets/img/collections/funparc.jpg",
-      title: "Disco #3",
+      title: "Disco - Funparc",
       hovered: false,
     },
     {
       src: "assets/img/collections/mining.jpg",
-      title: "Web Mining #1",
+      title: "Web - Shutdown System #1",
       hovered: false,
     },
     {
       src: "assets/img/collections/report1.jpg",
-      title: "Report #1",
+      title: "Web - Shutdown System #2",
       hovered: false,
     },
     {
       src: "assets/img/collections/report2.jpg",
-      title: "Report #2",
+      title: "Web - Shutdown System #3",
       hovered: false,
     },
     {
       src: "assets/img/collections/report3.jpg",
-      title: "Report #3",
+      title: "Web - Shutdown System #4",
       hovered: false,
     },
     {
@@ -58,6 +53,11 @@ export const COLLECTION_LIST = {
     {
       src: "assets/img/collections/flutter-web.png",
       title: "Admin Management - Flutter Web",
+      hovered: false,
+    },
+    {
+      src: "assets/img/collections/traveljuanders1.png",
+      title: "Mobile - Travel Juanders",
       hovered: false,
     },
   ],
